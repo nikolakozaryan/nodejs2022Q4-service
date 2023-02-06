0d78f7f550ffb0e1f1d54669f3faf22a2b35826b
Nikolay <kolyak770@gmail.com>
2023-02-06 14:45:57 +0300

    feat: add PORT from .env file

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(4000);
+  await app.listen(PORT);
 }
 bootstrap();
